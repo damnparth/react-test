f0864c60e4d35b7d0177fac64888380e12e204cb
parth <dasparth1544@gmail.com>
2025-01-27 22:41:00 +0530

    Header

---
+++ react-to-do/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header()
+{
+    return(
+        <>
+        <ul className="Header">
+    
+        <li><button>Login</button></li>
+        <li><button>Signup</button></li>
+        </ul>
+       
+        </>
+    )
+}
+export default Header;
